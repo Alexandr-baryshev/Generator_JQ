89b7ea880147378f3faab191db04a3237633c329
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-05-01 23:54:45 +1000

    Save 2

--- src/main/java/testJQ/Generator_JQ/controllers/MainController.java
+++ src/main/java/testJQ/Generator_JQ/controllers/MainController.java
@@ -6,27 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// НАЧАЛО SAVE_JS
+
 @RestController
 public class MainController {
 
 //   GeneratorFunc gf = new GeneratorFunc();
 
 
-
-
-// ****** НАЧАЛО непонятной хрени ******** (непонятно что на что нужно заменить..)
 private static final String template = "Hello, %s!";
    private final AtomicLong counter = new AtomicLong();
 
-   @GetMapping("/")
+   @GetMapping("/hello")
    public GeneratorData gd(@RequestParam(value = "name", defaultValue = "World") String name) {
    return new GeneratorData(counter.incrementAndGet(), String.format(template, name));
-
-
-// ****** КОНЕЦ непонятной хрени ********
-
-
 
 
 
