baae2a3a0b2689c7dbaed6e0ea41a6badaf44267
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-05-01 15:31:32 +1000

    Save 2

--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -1,15 +1,40 @@
-$(document).ready(function() {
-    // $.ajax({
-    //     url: "http://rest-service.guides.spring.io/generatordata"
-    // }).then(function(data) {
-        $('butt_id').on('click', function () {
-            alert('РАБОТАЕТ');
+$('document').ready(function () {
+    $('#butt_id').on('click', function () {
+        // alert('РАБОТАТЕТ');
 
-// тут я делал по видео уроку, и хотел просто проверить, как кнопка реагирует на нажатие,
-// но она не реагирует
+        var inputTxt, startR, sizeR, startC, sizeC
 
-        });
-        // $('inputTxt_id').append(data.id);
+        inputTxt = $('#inputTxt_id').val()
+
+        startR = $('#startR_id').val()
+        startR = parseInt(startR)
+
+        sizeR = $('#sizeR_id').val()
+        sizeR = parseInt(sizeR)
+
+        startC = $('#startC_id').val()
+        startC = parseInt(startC)
+
+        sizeC = $('#sizeC_id').val()
+        sizeC = parseInt(sizeC)
+
+
+        $('#outTxt_id').val(sort_R(inputTxt,startR,sizeR, startC, sizeC))
 
     });
-// });
+});
+
+
+function sort_R(inputTxtp, startRp, sizeRp, startCp, sizeCp) { //
+    let row, column, allResult = ""
+
+    for (let r = startRp; r <= sizeRp; r++) {
+        row = inputTxtp.replace("R1", "R" + r)
+
+        for (let c = startCp; c <= sizeCp; c += 1) {
+            column = row.replace("C1", "C" + c);
+            allResult = allResult + column + "\n";
+        }
+    }
+    return (allResult)
+}
