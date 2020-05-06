2e8868edca34da4a0f7a1f890a5960e9f96ebc6f
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-05-06 16:38:21 +1000

    Save 2

---
+++ src/main/java/testJQ/Generator_JQ/controllers/GeneratorDataRepository.java
@@ -0,0 +1,7 @@
+package testJQ.Generator_JQ.controllers;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface GeneratorDataRepository extends MongoRepository<GeneratorData, String> {
+
+}
