16c56aefcefefe810bc7546e5f4e8dd92594d8de
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-05-26 12:07:27 +1000

    Save 2

--- src/main/resources/static/js/generator.js
+++ src/main/resources/static/js/generator.js
@@ -1,102 +1,12 @@
-// ********** JQuery - Ajax  ************************
 
-// Выполнить
-$(document).ready(function () {
+$(document).ready(function (){
+
    $('#butt_id').on('click', function () {
-
-      let gdParam = {
-         inputTxt: null,
-         outTxt: null,
-
-         startR: null,
-         sizeR: null,
-         stepR: null,
-         targetR: null,
-         replaceR: null,
-
-         startC: null,
-         sizeC: null,
-         stepC: null,
-         targetC: null,
-         replaceC: null,
-
-         positX: null,
-         positY: null,
-         width: null,
-         height: null
-
-      };
-
-      gdParam.inputTxt = $('#inputTxt_id').val()
-      if (gdParam.inputTxt === "") {
-         gdParam.inputTxt = "R1 C1"
-      }
-
-      gdParam.outTxt = $('#outTxt_id').val()
-
-
-      gdParam.startR = $('#startR_id').val()
-      if (gdParam.startR === "") {
-         gdParam.startR = 1
-      }
-
-      gdParam.sizeR = $('#sizeR_id').val()
-      if (gdParam.sizeR === "") {
-         gdParam.sizeR = 1
-      }
-
-      gdParam.stepR = $('#stepR_id').val()
-      if (gdParam.stepR === "") {
-         gdParam.stepR = 1
-      }
-
-      gdParam.targetR = $('#targetR_id').val()
-      if (gdParam.targetR === "") {
-         gdParam.targetR = "R1"
-      }
-
-      gdParam.replaceR = $('#replaceR_id').val()
-      if (gdParam.replaceR === "") {
-         gdParam.replaceR = "R"
-      }
-
-
-      gdParam.startC = $('#startC_id').val()
-      if (gdParam.startC === "") {
-         gdParam.startC = 1
-      }
-
-      gdParam.sizeC = $('#sizeC_id').val()
-      if (gdParam.sizeC === "") {
-         gdParam.sizeC = 1
-      }
-
-      gdParam.stepC = $('#stepC_id').val()
-      if (gdParam.stepC === "") {
-         gdParam.stepC = 1
-      }
-
-      gdParam.targetC = $('#targetC_id').val()
-      if (gdParam.targetC === "") {
-         gdParam.targetC = "C1"
-      }
-
-      gdParam.replaceC = $('#replaceC_id').val()
-      if (gdParam.replaceC === "") {
-         gdParam.replaceC = "C"
-      }
-
-
-      gdParam.positX = $('#positX_id').val()
-      gdParam.positY = $('#positY_id').val()
-      gdParam.width = $('#width_id').val()
-      gdParam.height = $('#height_id').val()
 
       $.ajax({
          type: "POST",
          url: "/data",
-         // The key needs to match your method's input parameter (case-sensitive).
-         data: JSON.stringify(gdParam, null, 2),
+         data: JSON.stringify(gData(), null, 2),
          contentType: "application/json; charset=utf-8",
          dataType: "json",
          success: function (dataX) {
@@ -108,106 +18,14 @@ $(document).ready(function () {
          }
       });
    });
-});
 
-// Выполнить и сохранить
-$(document).ready(function () {
+
    $('#buttSave_id').on('click', function () {
-
-      let gdParam = {
-         inputTxt: null,
-         outTxt: null,
-
-         startR: null,
-         sizeR: null,
-         stepR: null,
-         targetR: null,
-         replaceR: null,
-
-
-         startC: null,
-         sizeC: null,
-         stepC: null,
-         targetC: null,
-         replaceC: null,
-
-         positX: null,
-         positY: null,
-         width: null,
-         height: null
-
-      };
-
-      gdParam.inputTxt = $('#inputTxt_id').val()
-      if (gdParam.inputTxt === "") {
-         gdParam.inputTxt = "R1 C1"
-      }
-
-      gdParam.outTxt = $('#outTxt_id').val()
-
-
-      gdParam.startR = $('#startR_id').val()
-      if (gdParam.startR === "") {
-         gdParam.startR = 1
-      }
-
-      gdParam.sizeR = $('#sizeR_id').val()
-      if (gdParam.sizeR === "") {
-         gdParam.sizeR = 1
-      }
-
-      gdParam.stepR = $('#stepR_id').val()
-      if (gdParam.stepR === "") {
-         gdParam.stepR = 1
-      }
-
-      gdParam.targetR = $('#targetR_id').val()
-      if (gdParam.targetR === "") {
-         gdParam.targetR = "R1"
-      }
-
-      gdParam.replaceR = $('#replaceR_id').val()
-      if (gdParam.replaceR === "") {
-         gdParam.replaceR = "R"
-      }
-
-
-      gdParam.startC = $('#startC_id').val()
-      if (gdParam.startC === "") {
-         gdParam.startC = 1
-      }
-
-      gdParam.sizeC = $('#sizeC_id').val()
-      if (gdParam.sizeC === "") {
-         gdParam.sizeC = 1
-      }
-
-      gdParam.stepC = $('#stepC_id').val()
-      if (gdParam.stepC === "") {
-         gdParam.stepC = 1
-      }
-
-      gdParam.targetC = $('#targetC_id').val()
-      if (gdParam.targetC === "") {
-         gdParam.targetC = "C1"
-      }
-
-      gdParam.replaceC = $('#replaceC_id').val()
-      if (gdParam.replaceC === "") {
-         gdParam.replaceC = "C"
-      }
-
-
-      gdParam.positX = $('#positX_id').val()
-      gdParam.positY = $('#positY_id').val()
-      gdParam.width = $('#width_id').val()
-      gdParam.height = $('#height_id').val()
 
       $.ajax({
          type: "POST",
          url: "/dataSave",
-         // The key needs to match your method's input parameter (case-sensitive).
-         data: JSON.stringify(gdParam, null, 2),
+         data: JSON.stringify(gData(), null, 2),
          contentType: "application/json; charset=utf-8",
          dataType: "json",
          success: function (dataX) {
@@ -222,43 +40,97 @@ $(document).ready(function () {
 });
 
 
-// ****** Просто JQuery  ******
+function gData() {
+   // Создание "data класса"
+   let gd = {
+      inputTxt: null,
+      outTxt: null,
 
-// $('document').ready(function () {
-//     $('#butt_id').on('click', function () {
-//         // alert('РАБОТАТЕТ');
-//
-//         var inputTxt, startR, sizeR, startC, sizeC
-//
-//         inputTxt = $('#inputTxt_id').val()
-//
-//         startR = $('#startR_id').val()
-//         startR = parseInt(startR)
-//
-//         sizeR = $('#sizeR_id').val()
-//         sizeR = parseInt(sizeR)
-//
-//         startC = $('#startC_id').val()
-//         startC = parseInt(startC)
-//
-//         sizeC = $('#sizeC_id').val()
-//         sizeC = parseInt(sizeC)
-//
-//         $('#outTxt_id').val(sort_R(inputTxt, startR, sizeR, startC, sizeC))
-//     });
-// });
-//
-//
-// function sort_R(inputTxtp, startRp, sizeRp, startCp, sizeCp) { //
-//     let row, column, allResult = ""
-//
-//     for (let r = startRp; r <= sizeRp; r++) {
-//         row = inputTxtp.replace("R1", "R" + r)
-//
-//         for (let c = startCp; c <= sizeCp; c += 1) {
-//             column = row.replace("C1", "C" + c);
-//             allResult = allResult + column + "\n";
-//         }
-//     }
-//     return (allResult)
-// }
+      startR: null,
+      sizeR: null,
+      stepR: null,
+      targetR: null,
+      replaceR: null,
+
+      startC: null,
+      sizeC: null,
+      stepC: null,
+      targetC: null,
+      replaceC: null,
+
+      positX: null,
+      positY: null,
+      width: null,
+      height: null
+   };
+
+   //Присваивание значений RC
+   {
+      gd.inputTxt = $('#inputTxt_id').val()
+      if (gd.inputTxt === "") {
+         gd.inputTxt = "R1 C1"
+      }
+
+      gd.outTxt = $('#outTxt_id').val()
+
+
+      gd.startR = $('#startR_id').val()
+      if (gd.startR === "") {
+         gd.startR = 1
+      }
+
+      gd.sizeR = $('#sizeR_id').val()
+      if (gd.sizeR === "") {
+         gd.sizeR = 1
+      }
+
+      gd.stepR = $('#stepR_id').val()
+      if (gd.stepR === "") {
+         gd.stepR = 1
+      }
+
+      gd.targetR = $('#targetR_id').val()
+      if (gd.targetR === "") {
+         gd.targetR = "R1"
+      }
+
+      gd.replaceR = $('#replaceR_id').val()
+      if (gd.replaceR === "") {
+         gd.replaceR = "R"
+      }
+
+
+      gd.startC = $('#startC_id').val()
+      if (gd.startC === "") {
+         gd.startC = 1
+      }
+
+      gd.sizeC = $('#sizeC_id').val()
+      if (gd.sizeC === "") {
+         gd.sizeC = 1
+      }
+
+      gd.stepC = $('#stepC_id').val()
+      if (gd.stepC === "") {
+         gd.stepC = 1
+      }
+
+      gd.targetC = $('#targetC_id').val()
+      if (gd.targetC === "") {
+         gd.targetC = "C1"
+      }
+
+      gd.replaceC = $('#replaceC_id').val()
+      if (gd.replaceC === "") {
+         gd.replaceC = "C"
+      }
+
+      gd.positX = $('#positX_id').val()
+      gd.positY = $('#positY_id').val()
+      gd.width = $('#width_id').val()
+      gd.height = $('#height_id').val()
+
+      return gd
+   }
+}
+
