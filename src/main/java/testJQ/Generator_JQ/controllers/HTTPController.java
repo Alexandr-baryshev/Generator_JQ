5b1a21ffd0df900f5970f914f25ca006c92c20f6
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-07-24 11:55:51 +1000

    Save

--- src/main/java/testJQ/Generator_JQ/controllers/HTTPController.java
+++ src/main/java/testJQ/Generator_JQ/controllers/HTTPController.java
@@ -8,14 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HTTPController {
 
-   @GetMapping("/")
-   public String generator(Model model) {
-      return "generator";
-   }
+//   @GetMapping("/")
+//   public String index(Model model) {
+//      return "index";
+//   }
 
    @GetMapping("/history")
    public String history(Model model) {
       return "history";
+   }
+
+   @GetMapping("/generator")
+   public String generator(Model model) {
+      return "generator";
    }
 
 }
